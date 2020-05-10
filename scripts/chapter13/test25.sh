83d00b4f4dd3e9358ad729a1c03e2cfa3ca291e4
LiyangHao <liyanghao19890724@163.com>
2020-05-10 19:03:57 +0800

    Create test25.sh

---
+++ scripts/chapter13/test25.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# finding files in the PATH
+
+IFS.OLD=$IFS
+IFS=:
+for folder in $PATH
+do
+    echo "$folder:"
+    for file in $folder/*
+    do
+        if [ -x $file ]
+        then
+            echo "  $file"
+        fi
+    done
+done
+IFS=$IFS.OLD
